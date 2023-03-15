f6b5bcc47b24f4cafe0d0ae5a1138e2c78abfb5c
sriharshakandala <sriharsha.kvs@gmail.com>
2023-03-15 13:17:15 -0700

    Address reviewer comment

--- src/mpi.jl
+++ src/mpi.jl
@@ -193,7 +193,7 @@ graph_context(
 )
 
 function start(ghost::MPISendRecvGraphContext; dependencies = nothing)
-    if !all(r -> r == MPI.isnull, ghost.recv_reqs)
+    if !all(MPI.isnull, ghost.recv_reqs)
         error("Must finish() before next start()")
     end
     # post receives
