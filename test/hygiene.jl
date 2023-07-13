ea983ecd56aeeefb1db2a28d0516592e94823130
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-07-13 10:37:22 -0700

    Improve hygiene test
    Fix macro hygiene
    
    Bump patch version

--- test/hygiene.jl
+++ test/hygiene.jl
@@ -1,13 +1,20 @@
 import ClimaComms as CC
+function test_macro_hyhiene(dev)
+    n = 3 # tests that we can reach variables defined in scope
+    CC.@threaded dev for i in 1:n
+        if Threads.nthreads() > 1
+            @show Threads.threadid()
+        end
+    end
+
+    CC.@time dev for i in 1:n
+        sin.(rand(10))
+    end
+
+    CC.@elapsed dev for i in 1:n
+        sin.(rand(10))
+    end
+end
 dev = CC.device()
-CC.@threaded dev for i in 1:2
-    1
-end
 
-CC.@time dev for i in 1:2
-    sin.(rand(10))
-end
-
-CC.@elapsed dev for i in 1:2
-    sin.(rand(10))
-end
+test_macro_hyhiene(dev)
